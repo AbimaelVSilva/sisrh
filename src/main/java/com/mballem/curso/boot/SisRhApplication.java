e290d1e1ee5e3eafa09a5aeda360653c200193ce
AbimaelVSilva <abimaelvsilva@gmail.com>
2018-12-30 20:26:45 -0300

    my first comit test sisrh app

---
+++ src/main/java/com/mballem/curso/boot/SisRhApplication.java
@@ -0,0 +1,14 @@
+package com.mballem.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisRhApplication.class, args);
+	}
+
+}
+
